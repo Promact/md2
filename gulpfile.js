061522e1f1e5835986177e3d5ef0116e509a915a
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-06-15 18:31:51 +0530

    added gulp task file.

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+"use strict";
+
+var gulp = require('gulp');
+var del = require('del');
+var tslint = require('gulp-tslint');
+var tsc = require('gulp-typescript');
+var sourcemaps = require('gulp-sourcemaps');
+var tsProject = tsc.createProject('tsconfig.json');
+
+gulp.task('clean', function (cb) {
+  return del(['build', 'src/**/*.js', '!src/systemjs.config.js', 'src/**/*.js.map', 'src/**/*.d.ts', '*.js', '!gulpfile.js', '!gulp.config.js', '*.js.map', '*.d.ts'], cb);
+});
+
+gulp.task('tslint', function (done) {
+  return gulp.src("src/**/*.ts")
+      .pipe(tslint({ configuration: "./tslint.json" }))
+      .pipe(tslint.report('prose', {
+        emitError: false
+      }));
+})
+
+gulp.task("compile", function () {
+  var tsResult = gulp.src("src/**/*.ts")
+      .pipe(sourcemaps.init())
+      .pipe(tsc(tsProject));
+
+  return tsResult.js
+      .pipe(sourcemaps.write("."))
+      .pipe(gulp.dest("build"));
+});
+
+gulp.task("resources", function () {
+  return gulp.src(["src/**/*", "!**/*.ts"])
+      .pipe(gulp.dest("build"));
+});
+
+gulp.task("libs", function () {
+  return gulp.src([
+          'es6-shim/es6-shim.min.js',
+          'systemjs/dist/system-polyfills.js',
+          'systemjs/dist/system.src.js',
+          'reflect-metadata/Reflect.js',
+          'rxjs/**',
+          'zone.js/dist/**',
+          '@angular/**'
+  ], { cwd: "node_modules/**" }) /* Glob required here. */
+      .pipe(gulp.dest("build/lib"));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(["src/**/*.ts"], ['compile']).on('change', function (e) {
+    console.log('TypeScript file ' + e.path + ' has been changed. Compiling.');
+  });
+  gulp.watch(["src/**/*.html", "src/**/*.css"], ['resources']).on('change', function (e) {
+    console.log('Resource file ' + e.path + ' has been changed. Updating.');
+  });
+});
+
+gulp.task("build", ['compile', 'resources', 'libs'], function () {
+  console.log("Building the project ...");
+});
+
+gulp.task("publish", function () {
+  var tsResult = gulp.src("src/**/*.ts")
+      .pipe(sourcemaps.init())
+      .pipe(tsc(tsProject));
+
+  return tsResult.js
+      .pipe(sourcemaps.write("."))
+      .pipe(gulp.dest("test"));
+});

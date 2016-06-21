ffb232397d8ea218e97927dab65471b9844044f2
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-06-21 15:26:55 +0530

    fixed: duplication tag issue in tags component.

--- src/components/tags/tags.ts
+++ src/components/tags/tags.ts
@@ -187,7 +187,7 @@ export class Md2Tags implements AfterContentInit, ControlValueAccessor {
     if (event.keyCode === 9) { return; }
     // Enter / Space
     if (event.keyCode === 13 || event.keyCode === 32) {
-      if (!this.tagBuffer || !this.isMenuVisible) return;
+      if (!this.tagBuffer || !this.isMenuVisible) { event.preventDefault(); return; }
       event.preventDefault();
       this.addTag(event, this.focusedTag);
       return;

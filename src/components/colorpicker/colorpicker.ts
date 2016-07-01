93a2cc635f678fe546f46e818feee94dffbcf3c7
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-07-01 14:33:15 +0530

    updated defoult color in colorpicker component.

--- src/components/colorpicker/colorpicker.ts
+++ src/components/colorpicker/colorpicker.ts
@@ -24,7 +24,7 @@ export class Md2Colorpicker implements OnInit {
   }
 
   ngOnInit() {
-    if (!this.colorpicker) { this.colorpicker = '#fff'; }
+    if (!this.colorpicker) { this.colorpicker = '#000'; }
     let hsva: any = this.service.stringToHsva(this.colorpicker);
     if (hsva !== null) {
       this.colorpickerChange.emit(this.service.outputFormat(hsva, this.format));

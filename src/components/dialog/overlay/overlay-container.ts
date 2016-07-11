3411fad48515c0940352fe54c404ad9652a0cf49
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-07-11 10:33:12 +0530

    update implementation of dialog component.

--- src/components/dialog/overlay/overlay-container.ts
+++ src/components/dialog/overlay/overlay-container.ts
@@ -1,9 +1,3 @@
-
-
-/**
- * Create the overlay container element, which is simply a div
- * with the 'md-overlay-container' class on the document body.
- */
 export function createOverlayContainer(): Element {
     let container = document.createElement( 'div' );
     container.classList.add( 'md2-overlay' );

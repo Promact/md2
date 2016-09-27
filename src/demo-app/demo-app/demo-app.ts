ca94b37417c8465fe39d5c6413dfb28e3dec59ca
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-09-27 13:55:42 +0530

    demo: design update.

--- src/demo-app/demo-app/demo-app.ts
+++ src/demo-app/demo-app/demo-app.ts
@@ -14,13 +14,13 @@ import {Location} from '@angular/common';
       <a button="primary" href="https://github.com/Promact/md2/zipball/master">Download .zip</a>
       <a button="primary" href="https://github.com/Promact/md2/tarball/master">Download .tar.gz</a>
       <h1>Getting started</h1>
-      <h3>Dependencies</h3>
+      <h4>Dependencies</h4>
       <p>This module consists of native Angular2 components, directives and services, no jQuery, Material or Bootstrap javascript is required.</p>
       <p>Plus this module plays nice with Material Design CSS</p>
-      <h3>Installation</h3>
+      <h4>Installation</h4>
       <p>Currently preferable way to install this module is <code>npm</code>:</p>
       <pre class="language-bash"><code class="language-bash">npm <span class="token function">install</span> --save md2</code></pre>
-      <h3>Reading documentation</h3>
+      <h4>Reading documentation</h4>
       <p>Each <code>MD2</code> components has api and annotation docs, examples and working demo. Each <code>property</code> and <code>event</code> has type annotation and default value if any.</p>
     </div>
   `

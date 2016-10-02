5104b1c33b7cd213c26e5b9c3452e854d5e6fb06
Dharmesh Pipariya <pipariyadharmesh@gmail.com>
2016-10-02 16:06:47 +0530

    update select demo

--- src/demo-app/select/select-demo.ts
+++ src/demo-app/select/select-demo.ts
@@ -6,19 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: 'select-demo.html'
 })
 export class SelectDemo {
-  private test: any;
-  private tests: Array<any> =
-  [
-    { name: 'Vadodara', value: '1', disabled: false },
-    { name: 'Rajkot', value: '2', disabled: false },
-    { name: 'Delhi', value: '3', disabled: false },
-    { name: 'Chennai', value: '4', disabled: true },
-    { name: 'Mumbai', value: '5', disabled: false },
-    { name: 'Goa', value: '6', disabled: true }
-  ];
-
-
-
   private items: Array<any> =
   [
     { name: 'Vadodara', value: '1', disabled: false },
@@ -28,16 +15,8 @@ export class SelectDemo {
     { name: 'Mumbai', value: '5', disabled: false },
     { name: 'Goa', value: '6', disabled: true }
   ];
-  private item: any = '3';
+  private item: string = '3';
   private change(value: any) {
     console.log('Changed data: ', value);
-  }
-  private y = { name: 'x' }
-  as() {
-    this.item = this.y;
-
-  }
-  ys() {
-    this.item = null;
   }
 }

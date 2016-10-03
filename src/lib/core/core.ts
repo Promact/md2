e249612f519d38a0f9ead2f496151afd215a10ee
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-10-03 11:17:28 +0530

    update core module

--- src/lib/core/core.ts
+++ src/lib/core/core.ts
@@ -1,87 +1,57 @@
-import {NgModule, ModuleWithProviders} from '@angular/core';
-//import {MdLineModule} from './line/line';
-//import {RtlModule} from './rtl/dir';
-//import {MdRippleModule} from './ripple/ripple';
-import {PortalModule} from './portal/portal-directives';
-import {OverlayModule} from './overlay/overlay-directives';
-//import {MdLiveAnnouncer} from './a11y/live-announcer';
-
-// RTL
-//export {Dir, LayoutDirection, RtlModule} from './rtl/dir';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { PortalModule } from './portal/portal-directives';
+import { OverlayModule } from './overlay/overlay-directives';
 
 // Portals
 export {
-Portal,
-PortalHost,
-BasePortalHost,
-ComponentPortal,
-TemplatePortal
+  Portal,
+  PortalHost,
+  BasePortalHost,
+  ComponentPortal,
+  TemplatePortal
 } from './portal/portal';
 export {
-PortalHostDirective,
-TemplatePortalDirective,
-PortalModule,
+  PortalHostDirective,
+  TemplatePortalDirective,
+  PortalModule,
 } from './portal/portal-directives';
-export {DomPortalHost} from './portal/dom-portal-host';
+export { DomPortalHost } from './portal/dom-portal-host';
 
 // Overlay
-export {Overlay, OVERLAY_PROVIDERS} from './overlay/overlay';
-export {OverlayContainer} from './overlay/overlay-container';
-export {OverlayRef} from './overlay/overlay-ref';
-export {OverlayState} from './overlay/overlay-state';
+export { Overlay, OVERLAY_PROVIDERS } from './overlay/overlay';
+export { OverlayContainer } from './overlay/overlay-container';
+export { OverlayRef } from './overlay/overlay-ref';
+export { OverlayState } from './overlay/overlay-state';
 export {
-ConnectedOverlayDirective,
-OverlayOrigin,
-OverlayModule,
+  ConnectedOverlayDirective,
+  OverlayOrigin,
+  OverlayModule,
 } from './overlay/overlay-directives';
 export * from './overlay/position/connected-position-strategy';
 export * from './overlay/position/connected-position';
 
-// Gestures
-//export {MdGestureConfig} from './gestures/MdGestureConfig';
-
-// Ripple
-//export {MdRipple, MdRippleModule} from './ripple/ripple';
-
-// a11y
-//export {
-//  AriaLivePoliteness,
-//  MdLiveAnnouncer,
-//  LIVE_ANNOUNCER_ELEMENT_TOKEN,
-//} from './a11y/live-announcer';
-
-export {
-MdUniqueSelectionDispatcher,
-MdUniqueSelectionDispatcherListener
-} from './coordination/unique-selection-dispatcher';
-
-//export {MdLineModule, MdLine, MdLineSetter} from './line/line';
-
 // Style
-export {applyCssTransform} from './style/apply-transform';
+export { applyCssTransform } from './style/apply-transform';
 
 // Error
-export {MdError} from './errors/error';
-
-// Annotations.
-//export {BooleanFieldValue} from './annotations/field-value';
+export { MdError } from './errors/error';
 
 // Misc
-export {ComponentType} from './overlay/generic-component-type';
+export { ComponentType } from './overlay/generic-component-type';
 
 // Keybindings
 export * from './keyboard/keycodes';
 
 
 @NgModule({
-  imports: [PortalModule, OverlayModule],//MdLineModule, RtlModule, MdRippleModule,
-  exports: [PortalModule, OverlayModule],//MdLineModule, RtlModule, MdRippleModule,
+  imports: [PortalModule, OverlayModule],
+  exports: [PortalModule, OverlayModule],
 })
 export class MdCoreModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: MdCoreModule,
-      providers: []//MdLiveAnnouncer
+      providers: []
     };
   }
 }

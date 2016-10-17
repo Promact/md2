639ba3dadc98860f5127636a8a84df8999ec7201
krupa <krupa@promactinfo.com>
2016-10-17 14:43:59 +0530

    remove extra white space

--- src/demo-app/chips/chips-demo.ts
+++ src/demo-app/chips/chips-demo.ts
@@ -1,24 +1,24 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'md-chips-demo',
   templateUrl: 'chips-demo.html'
 })
-export class ChipsDemo { 
-  
-    private items: Array<any> =
-    [
-      'Data1', 'Data2', 'Data3', 'Data4', 'Data5', 'Data6', 'Data7', 'Data8'
-    ];
-    private itemsData: Array<any> =
-    [
-      'Data1', 'Data2', 'Data3'
-    ];
-    
-    private validPattern =/^[0-9]*$/;
+export class ChipsDemo {
 
-    private change(value: any) {
-      console.log('Changed data: ', value);
-    }
+  private items: Array<any> =
+  [
+    'Data1', 'Data2', 'Data3', 'Data4', 'Data5', 'Data6', 'Data7', 'Data8'
+  ];
+  private itemsData: Array<any> =
+  [
+    'Data1', 'Data2', 'Data3'
+  ];
+
+  private validPattern = /^[0-9]*$/;
+
+  private change(value: any) {
+    console.log('Changed data: ', value);
+  }
 }

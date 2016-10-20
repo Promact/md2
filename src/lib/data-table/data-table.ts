3f2a15b2dd7fb2552a146ec64771bd0aa953bc56
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-10-20 11:56:24 +0530

    fix(data-table) pagination

--- src/lib/data-table/data-table.ts
+++ src/lib/data-table/data-table.ts
@@ -84,8 +84,8 @@ export class Md2DataTable implements OnChanges, DoCheck {
   }
 
   private recalculatePage() {
-    let lastPage = Math.ceil(this.inputData.length / this.pageLength);
-    this.activePage = lastPage < this.activePage ? lastPage : this.activePage;
+    let _lastPage = Math.ceil(this.inputData.length / this.pageLength);
+    this.activePage = _lastPage < this.activePage ? _lastPage : this.activePage;
     this.activePage = this.activePage || 1;
   }
 
@@ -116,16 +116,14 @@ export class Md2DataTable implements OnChanges, DoCheck {
     let offset = (this.activePage - 1) * this.pageLength;
     let data = this.inputData;
     let sortField = this.sortField;
-    //if (typeof sortField === 'string' || sortField instanceof String) {
-    //  //data = _.orderBy(data, this.caseInsensitiveIteratee(<string>sortField), [this.sortOrder]);
-    //} else {
-    //  //data = _.orderBy(data, sortField, [this.sortOrder]);
-    //}
-    //if (sortField) {
-    //  data = data.sort((a, b) => {
-    //    return (a[sortField + ''].toLowerCase() > b[sortField + ''].toLowerCase()) ? 1 : ((b[sortField + ''].toLowerCase() > a[sortField + ''].toLowerCase()) ? -1 : 0) * (this.sortOrder == 'desc' ? -1 : 1);
-    //  });
-    //}
+    if (sortField) {
+      data = data.sort((a, b) => {
+        let x = isNaN(a[sortField + '']) ? a[sortField + ''].toLowerCase() : a[sortField + ''];
+        let y = isNaN(b[sortField + '']) ? b[sortField + ''].toLowerCase() : b[sortField + ''];
+        return (x > y) ? 1 : (y > x) ? -1 : 0;
+      });
+    }
+    if (this.sortOrder === 'desc') { data.reverse(); }
     this.data = data.slice(offset, offset + this.pageLength);
   }
 
@@ -148,25 +146,27 @@ export class Md2DataTable implements OnChanges, DoCheck {
       <svg *ngIf="isAsc" width="24"height="24" viewBox="0 0 24 24">
         <path d="M7 14l5-5 5 5z"/>
       </svg>
-      <svg *ngIf="!isAsc" width="24"height="24" viewBox="0 0 24 24">
+      <svg *ngIf="isDesc" width="24"height="24" viewBox="0 0 24 24">
         <path d="M7 10l5 5 5-5z"/>
       </svg>
     </span>
   `,
   styles: [`
     [md2-sort-field] span { position: relative; display: block; line-height: 24px; cursor: pointer; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; }
-    [md2-sort-field] span svg { display: inline-block; vertical-align: middle; }
+    [md2-sort-field] span svg { display: inline-block; vertical-align: middle; fill: currentColor; }
   `],
   encapsulation: ViewEncapsulation.None
 })
 export class Md2DataTableSortField {
   private isAsc: boolean = false;
+  private isDesc: boolean = false;
 
   @Input('md2-sort-field') private sortField: string;
 
   public constructor(private _md2Table: Md2DataTable) {
     _md2Table.onSortChange.subscribe((event: SortEvent) => {
-      this.isAsc = (event.sortField === this.sortField && event.sortOrder === "asc") ? true : false;
+      this.isAsc = (event.sortField === this.sortField && event.sortOrder === "asc");
+      this.isDesc = (event.sortField === this.sortField && event.sortOrder === "desc");
     })
   }
 
@@ -182,33 +182,33 @@ export class Md2DataTableSortField {
 @Component({
   selector: 'md2-pagination',
   template: `
-    <ul class="md2-pagination" *ngIf="dataLength > rowsOnPage">
-      <li [class.disabled]="activePage <= 1" (click)="_setPage(1)">
+    <ul class="md2-pagination" *ngIf="dataLength > _rows">
+      <li [class.disabled]="_activePage <= 1" (click)="_setPage(1)">
         <svg width="24" height="24" viewBox="0 0 24 24">
           <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/>
         </svg>
       </li>
-      <li *ngIf="activePage > 4 && activePage + 1 > lastPage" (click)="_setPage(activePage - 4)">{{activePage-4}}</li>
-      <li *ngIf="activePage > 3 && activePage + 2 > lastPage" (click)="_setPage(activePage - 3)">{{activePage-3}}</li>
-      <li *ngIf="activePage > 2" (click)="_setPage(activePage - 2)">{{activePage-2}}</li>
-      <li *ngIf="activePage > 1" (click)="_setPage(activePage - 1)">{{activePage-1}}</li>
-      <li class="active">{{activePage}}</li>
-      <li *ngIf="activePage + 1 <= lastPage" (click)="_setPage(activePage + 1)">{{activePage+1}}</li>
-      <li *ngIf="activePage + 2 <= lastPage" (click)="_setPage(activePage + 2)">{{activePage+2}}</li>
-      <li *ngIf="activePage + 3 <= lastPage && activePage < 3" (click)="_setPage(activePage + 3)">{{activePage+3}}</li>
-      <li *ngIf="activePage + 4 <= lastPage && activePage < 2" (click)="_setPage(activePage + 4)">{{activePage+4}}</li>
-      <li [class.disabled]="activePage >= lastPage" (click)="_setPage(lastPage)">
+      <li *ngIf="_activePage > 4 && _activePage + 1 > _lastPage" (click)="_setPage(_activePage - 4)">{{_activePage-4}}</li>
+      <li *ngIf="_activePage > 3 && _activePage + 2 > _lastPage" (click)="_setPage(_activePage - 3)">{{_activePage-3}}</li>
+      <li *ngIf="_activePage > 2" (click)="_setPage(_activePage - 2)">{{_activePage-2}}</li>
+      <li *ngIf="_activePage > 1" (click)="_setPage(_activePage - 1)">{{_activePage-1}}</li>
+      <li class="active">{{_activePage}}</li>
+      <li *ngIf="_activePage + 1 <= _lastPage" (click)="_setPage(_activePage + 1)">{{_activePage+1}}</li>
+      <li *ngIf="_activePage + 2 <= _lastPage" (click)="_setPage(_activePage + 2)">{{_activePage+2}}</li>
+      <li *ngIf="_activePage + 3 <= _lastPage && _activePage < 3" (click)="_setPage(_activePage + 3)">{{_activePage+3}}</li>
+      <li *ngIf="_activePage + 4 <= _lastPage && _activePage < 2" (click)="_setPage(_activePage + 4)">{{_activePage+4}}</li>
+      <li [class.disabled]="_activePage >= _lastPage" (click)="_setPage(_lastPage)">
         <svg width="24" height="24" viewBox="0 0 24 24">
           <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"/>
         </svg>
       </li>
     </ul>
     <ul class="md2-pagination pull-right" *ngIf="dataLength > _minRows">
-      <li *ngFor="let row of rows" [class.active]="rowsOnPage===row" (click)="_setRows(row)">{{row}}</li>
+      <li *ngFor="let row of rows" [class.active]="_rows===row" (click)="_setRows(row)">{{row}}</li>
     </ul>
   `,
   styles: [`
-    md2-pagination { display: block; color: #0e59a5; }
+    md2-pagination { display: block; color: #0e59a5; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; }
     md2-pagination .md2-pagination { display: inline-block; margin: .5rem 0; padding: 0; }
     md2-pagination .md2-pagination li { position: relative; display: inline-block; width: 36px; vertical-align: top; text-align: center; line-height: 36px; border-radius: 100px; cursor: pointer; box-sizing: border-box; }
     md2-pagination .md2-pagination li:hover { background: rgba(0,0,0,0.12); }
@@ -222,12 +222,12 @@ export class Md2DataTableSortField {
 })
 export class Md2Pagination implements OnChanges {
 
-  private md2Table: Md2DataTable;
+  private _md2Table: Md2DataTable;
   private _minRows = 0;
-  private activePage: number;
-  private rowsOnPage: number;
+  private _activePage: number;
+  private _rows: number;
+  private _lastPage: number;
   private dataLength: number = 0;
-  private lastPage: number;
 
   @Input('md2-rows') rows: any = [];
   @Input('md2-table') md2InputTable: Md2DataTable;
@@ -238,24 +238,24 @@ export class Md2Pagination implements OnChanges {
     if (changes.rows) {
       //this._minRows = _.min(this.rows)
     }
-    this.md2Table = this.md2InputTable || this.injectMd2Table;
-    this._onPageChange(this.md2Table.getPage());
-    this.md2Table.onPageChange.subscribe(this._onPageChange);
+    this._md2Table = this.md2InputTable || this.injectMd2Table;
+    this._onPageChange(this._md2Table.getPage());
+    this._md2Table.onPageChange.subscribe(this._onPageChange);
   }
 
   private _setPage(page: number): void {
-    this.md2Table.setPage(page, this.rowsOnPage);
+    this._md2Table.setPage(page, this._rows);
   }
 
   private _setRows(rows: number): void {
-    this.md2Table.setPage(this.activePage, rows);
+    this._md2Table.setPage(this._activePage, rows);
   }
 
-  private _onPageChange(event: PageEvent) {
-    this.activePage = event.activePage;
-    this.rowsOnPage = event.pageLength;
+  private _onPageChange = (event: PageEvent) => {
+    this._activePage = event.activePage;
+    this._rows = event.pageLength;
     this.dataLength = event.dataLength;
-    this.lastPage = Math.ceil(this.dataLength / this.rowsOnPage);
+    this._lastPage = Math.ceil(this.dataLength / this._rows);
   }
 }
 

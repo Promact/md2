84d6b101d270b8d72bd924cc1690801873416b5e
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-11-10 15:53:01 +0530

    deploy test

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="master"
+SOURCE_BRANCH="dev"
 TARGET_BRANCH="gh-pages"
 COMMIT_MSG=`git log --format=%B --no-merges -n 1`
 
@@ -17,7 +17,7 @@ echo "Match Comment"
 
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" -o "$COMMIT_MSG" != "deploy-"* ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" -o [ $COMMIT_MSG != "deploy-"* ]]; then
   echo "Building demo-app"
   gulp build:devapp
   exit 0

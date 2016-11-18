d8d0623d5d5a4e27e7d0bb7a0cf92d16d53c99cc
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-11-18 10:33:12 +0530

    update: demo-app module

--- src/demo-app/demo-app-module.ts
+++ src/demo-app/demo-app-module.ts
@@ -1,26 +1,26 @@
-import { NgModule, ApplicationRef } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
-import { DemoApp, Home } from './demo-app/demo-app';
-import { RouterModule } from '@angular/router';
-import { Md2Module } from 'md2';
-import { DEMO_APP_ROUTES } from './demo-app/routes';
-import { AccordionDemo } from './accordion/accordion-demo';
-import { AutocompleteDemo } from './autocomplete/autocomplete-demo';
-import { ChipsDemo } from './chips/chips-demo';
-import { CollapseDemo } from './collapse/collapse-demo';
-import { ColorpickerDemo } from './colorpicker/colorpicker-demo';
-import { DataTableDemo, DataTablePipe } from './data-table/data-table-demo';
-import { DatepickerDemo } from './datepicker/datepicker-demo';
-import { DialogDemo } from './dialog/dialog-demo';
-import { MenuDemo } from './menu/menu-demo';
-import { MultiselectDemo } from './multiselect/multiselect-demo';
-import { SelectDemo } from './select/select-demo';
-import { TabsDemo } from './tabs/tabs-demo';
-import { TagsDemo } from './tags/tags-demo';
-import { ToastDemo } from './toast/toast-demo';
-import { TooltipDemo } from './tooltip/tooltip-demo';
+import {NgModule, ApplicationRef} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {DemoApp, Home} from './demo-app/demo-app';
+import {RouterModule} from '@angular/router';
+import {Md2Module} from 'md2';
+import {DEMO_APP_ROUTES} from './demo-app/routes';
+import {AccordionDemo} from './accordion/accordion-demo';
+import {AutocompleteDemo} from './autocomplete/autocomplete-demo';
+import {ChipsDemo} from './chips/chips-demo';
+import {CollapseDemo} from './collapse/collapse-demo';
+import {ColorpickerDemo} from './colorpicker/colorpicker-demo';
+import {DataTableDemo, DataTablePipe} from './data-table/data-table-demo';
+import {DatepickerDemo} from './datepicker/datepicker-demo';
+import {DialogDemo} from './dialog/dialog-demo';
+import {MenuDemo} from './menu/menu-demo';
+import {MultiselectDemo} from './multiselect/multiselect-demo';
+import {SelectDemo} from './select/select-demo';
+import {TabsDemo} from './tabs/tabs-demo';
+import {TagsDemo} from './tags/tags-demo';
+import {ToastDemo} from './toast/toast-demo';
+import {TooltipDemo} from './tooltip/tooltip-demo';
 
 @NgModule({
   imports: [
@@ -28,6 +28,7 @@ import { TooltipDemo } from './tooltip/tooltip-demo';
     FormsModule,
     HttpModule,
     RouterModule.forRoot(DEMO_APP_ROUTES, { useHash: true }),
+    ReactiveFormsModule,
     Md2Module.forRoot(),
   ],
   declarations: [

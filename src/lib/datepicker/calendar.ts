1c4774afd8c416a356b519a8abf0b6bcdd43ead2
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-12-22 11:28:03 +0530

    chore(calendar): add calendar scaffold

---
+++ src/lib/datepicker/calendar.ts
@@ -0,0 +1,14 @@
+import {
+  Component,
+  ViewEncapsulation
+} from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'md2-calendar',
+  templateUrl: 'calendar.html',
+  styleUrls: ['calendar.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class Md2Calendar {
+}

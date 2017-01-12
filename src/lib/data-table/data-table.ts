90da4d7cbea1db15bd269ac493ef680a5fda5fd5
dharmeshpipariya <pipariyadharmesh@gmail.com>
2017-01-12 18:42:47 +0530

    chore(data-table) update core functionality

--- src/lib/data-table/data-table.ts
+++ src/lib/data-table/data-table.ts
@@ -45,25 +45,81 @@ export interface DataEvent {
 export class Md2DataTable implements OnChanges, DoCheck {
 
   private diff: IterableDiffer;
+  private isDataChanged = false;
 
-  @Input('md2Data') inputData: any[] = [];
-  @Input() rowsPerPage = 1000;
-  @Input() activePage = 1;
-  @Input() sortBy: string | string[] = '';
-  @Input() sortOrder = 'asc';
-
-  @Output() sortByChange = new EventEmitter<string | string[]>();
-  @Output() sortOrderChange = new EventEmitter<string>();
-
-  private mustRecalculateData = false;
+  private _activePage: number = 1;
 
   data: any[];
+
+  @Input() md2Data: any[] = [];
+  @Input() rowsPerPage: number = 1000;
+  //@Input() activePage: number = 1;
+  @Input() sortBy: string | string[] = '';
+  @Input() sortOrder: string = 'asc';
+
+  @Input()
+  get activePage() { return this._activePage; }
+  set activePage(value: number) {
+    console.log('Active Page: ' + value);
+    if (this._activePage !== value) {
+      this._activePage = value;
+    }
+  }
+
+  //@Input()
+  //get rowsPerPage() { return this._rowsPerPage; }
+  //set rowsPerPage(value: number) {
+  //  if (this._rowsPerPage !== value) {
+  //    this._rowsPerPage = value;
+  //  }
+  //}
+
+  @Output() activePageChange = new EventEmitter<number>();
+  @Output() sortByChange = new EventEmitter<string | string[]>();
+  @Output() sortOrderChange = new EventEmitter<string>();
 
   onSortChange = new ReplaySubject<SortEvent>(1);
   onPageChange = new EventEmitter<PageEvent>();
 
   constructor(private differs: IterableDiffers) {
     this.diff = differs.find([]).create(null);
+  }
+
+  ngOnChanges(changes: { [key: string]: SimpleChange }): any {
+    console.log('ngOnChanges');
+    if (changes['rowsPerPage']) {
+      this.rowsPerPage = changes['rowsPerPage'].currentValue;//.previousValue;
+      this.setPage(this.activePage, changes['rowsPerPage'].currentValue);
+      this.isDataChanged = true;
+    }
+    if (changes['sortBy'] || changes['sortOrder']) {
+      //if (!_.includes(['asc', 'desc'], this.sortOrder)) {
+      //  console.warn('md2-data-table: value md2SortOrder must be one of ["asc", "desc"], but is:', this.sortOrder);
+      //  this.sortOrder = 'asc';
+      //}
+      if (this.sortBy) {
+        this.onSortChange.next({ sortBy: this.sortBy, sortOrder: this.sortOrder });
+      }
+      this.isDataChanged = true;
+    }
+    if (changes['md2Data']) {
+      this.md2Data = changes['md2Data'].currentValue || [];
+      this.recalculatePage();
+      this.isDataChanged = true;
+    }
+  }
+
+  ngDoCheck(): any {
+    console.log('ngOnChanges');
+    let changes = this.diff.diff(this.md2Data);
+    if (changes) {
+      this.recalculatePage();
+      this.isDataChanged = true;
+    }
+    if (this.isDataChanged) {
+      this.fillData();
+      this.isDataChanged = false;
+    }
   }
 
   getSort(): SortEvent {
@@ -74,7 +130,7 @@ export class Md2DataTable implements OnChanges, DoCheck {
     if (this.sortBy !== sortBy || this.sortOrder !== sortOrder) {
       this.sortBy = sortBy;
       this.sortOrder = sortOrder;// _.includes(['asc', 'desc'], sortOrder) ? sortOrder : 'asc';
-      this.mustRecalculateData = true;
+      this.isDataChanged = true;
       this.onSortChange.next({ sortBy: sortBy, sortOrder: sortOrder });
       this.sortByChange.emit(this.sortBy);
       this.sortOrderChange.emit(this.sortOrder);
@@ -82,19 +138,20 @@ export class Md2DataTable implements OnChanges, DoCheck {
   }
 
   getPage(): PageEvent {
-    return { activePage: this.activePage, rowsPerPage: this.rowsPerPage, dataLength: this.inputData.length };
+    return { activePage: this.activePage, rowsPerPage: this.rowsPerPage, dataLength: this.md2Data.length };
   }
 
   setPage(activePage: number, rowsPerPage: number): void {
     if (this.rowsPerPage !== rowsPerPage || this.activePage !== activePage) {
       this.activePage = this.activePage !== activePage ? activePage : this.calculateNewActivePage(this.rowsPerPage, rowsPerPage);
       this.rowsPerPage = rowsPerPage;
-      this.mustRecalculateData = true;
+      this.isDataChanged = true;
       this.onPageChange.emit({
         activePage: this.activePage,
         rowsPerPage: this.rowsPerPage,
-        dataLength: this.inputData ? this.inputData.length : 0
+        dataLength: this.md2Data ? this.md2Data.length : 0
       });
+      this.activePageChange.emit(this.activePage);
     }
   }
 
@@ -105,58 +162,22 @@ export class Md2DataTable implements OnChanges, DoCheck {
   }
 
   private recalculatePage() {
-    let lastPage = Math.ceil(this.inputData.length / this.rowsPerPage);
+    let lastPage = Math.ceil(this.md2Data.length / this.rowsPerPage);
     this.activePage = lastPage < this.activePage ? lastPage : this.activePage;
     this.activePage = this.activePage || 1;
 
     this.onPageChange.emit({
       activePage: this.activePage,
       rowsPerPage: this.rowsPerPage,
-      dataLength: this.inputData.length
+      dataLength: this.md2Data.length
     });
   }
 
-  ngOnChanges(changes: { [key: string]: SimpleChange }): any {
-    if (changes['rowsPerPage']) {
-      this.rowsPerPage = changes['rowsPerPage'].previousValue;
-      this.setPage(this.activePage, changes['rowsPerPage'].currentValue);
-      this.mustRecalculateData = true;
-    }
-    if (changes['sortBy'] || changes['sortOrder']) {
-      //if (!_.includes(['asc', 'desc'], this.sortOrder)) {
-      //  console.warn('md2-data-table: value md2SortOrder must be one of ["asc", "desc"], but is:', this.sortOrder);
-      //  this.sortOrder = 'asc';
-      //}
-      if (this.sortBy) {
-        this.onSortChange.next({ sortBy: this.sortBy, sortOrder: this.sortOrder });
-      }
-      this.mustRecalculateData = true;
-    }
-    if (changes['inputData']) {
-      this.inputData = changes['inputData'].currentValue || [];
-      this.recalculatePage();
-      this.mustRecalculateData = true;
-    }
-  }
 
-  ngDoCheck(): any {
-    let changes = this.diff.diff(this.inputData);
-    if (changes) {
-      this.recalculatePage();
-      this.mustRecalculateData = true;
-    }
-    if (this.mustRecalculateData) {
-      this.fillData();
-      this.mustRecalculateData = false;
-    }
-  }
 
-  private fillData(): void {
-    this.activePage = this.activePage;
-    this.rowsPerPage = this.rowsPerPage;
-
+  private fillData() {
     let offset = (this.activePage - 1) * this.rowsPerPage;
-    let data = this.inputData;
+    let data = this.md2Data;
     let sortBy = this.sortBy;
     //if (typeof sortBy === 'string' || sortBy instanceof String) {
     //  data = _.orderBy(data, this.caseInsensitiveIteratee(<string>sortBy), [this.sortOrder]);
@@ -232,76 +253,36 @@ export class Md2Pagination {
 
   private _activePage: number = 1;
 
-  _onChange = (value: any) => { };
-  _onTouched = () => { };
-
-  //@Output() change: EventEmitter<Md2PaginationChange> = new EventEmitter<Md2PaginationChange>();
-
   @Input() rowsPerPageSet: any = [];
-  @Input('md2Table') md2DataTable: Md2DataTable;
-
-  //@Input()
-  get activePage() { return this._activePage; }
-  set activePage(value: number) {
-    if (this._activePage !== value) {
-      this._activePage = value;
-    }
-  }
-
-  minRowsPerPage = 0;
-
-  private mfTable: Md2DataTable;
+  @Input() md2Table: Md2DataTable;
 
   _rowsPerPage: number;
   _dataLength: number = 0;
   _lastPage: number;
 
-  constructor( @Optional() private _dataTable: Md2DataTable) {
-  }
+  constructor( @Optional() private _dataTable: Md2DataTable) { }
 
-  ngDoCheck() {//changes: any
-    //if (changes.rowsPerPageSet) {
-    //  this.minRowsPerPage = _.min(this.rowsPerPageSet)
-    //}
-    this.mfTable = this.md2DataTable || this._dataTable;
-    this.onPageChangeSubscriber(this.mfTable.getPage());
-    this.mfTable.onPageChange.subscribe(this.onPageChangeSubscriber);
+  ngDoCheck() {
+    this.md2Table = this.md2Table || this._dataTable;
+    this.onPageChangeSubscriber(this.md2Table.getPage());
+    this.md2Table.onPageChange.subscribe(this.onPageChangeSubscriber);
   }
 
   _setPage(pageNumber: number): void {
-    this.mfTable.setPage(pageNumber, this._rowsPerPage);
+    this.md2Table.setPage(pageNumber, this._rowsPerPage);
   }
 
   _setRows(event: any): void {
     event.stopPropagation();
-    this.mfTable.setPage(this.activePage, event.target.value);
+    this.md2Table.setPage(this._activePage, event.target.value);
   }
 
   private onPageChangeSubscriber = (event: PageEvent) => {
-    this.activePage = event.activePage;
+    this._activePage = event.activePage;
     this._rowsPerPage = event.rowsPerPage;
     this._dataLength = event.dataLength;
     this._lastPage = Math.ceil(this._dataLength / this._rowsPerPage);
   };
-
-
-  //_emitChangeEvent(): void {
-  //  let event = new Md2PaginationChange();
-  //  event.source = this;
-  //  event.activePage = this._activePage;
-  //  this._onChange(event.activePage);
-  //  this.change.emit(event);
-  //}
-
-  //writeValue(value: number): void {
-  //  if (this._activePage !== value) {
-  //    this._activePage = value;
-  //  }
-  //}
-
-  //registerOnChange(fn: (value: any) => void): void { this._onChange = fn; }
-
-  //registerOnTouched(fn: () => {}): void { this._onTouched = fn; }
 
 }
 

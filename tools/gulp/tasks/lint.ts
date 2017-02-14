b1e859f6682d530d12d77468fc324750ea31aca9
dharmeshpipariya <pipariyadharmesh@gmail.com>
2017-02-14 12:22:30 +0530

    chore: update tasks

--- tools/gulp/tasks/lint.ts
+++ tools/gulp/tasks/lint.ts
@@ -1,10 +1,15 @@
 import gulp = require('gulp');
-import {execNodeTask} from '../task_helpers';
-
+import { execNodeTask } from '../task_helpers';
 
 gulp.task('lint', ['tslint', 'stylelint', 'madge']);
+
+/** Task that runs madge to detect circular dependencies. */
 gulp.task('madge', ['build:release'], execNodeTask('madge', ['--circular', './dist']));
+
+/** Task to lint Angular Material's scss stylesheets. */
 gulp.task('stylelint', execNodeTask(
   'stylelint', ['src/**/*.scss', '--config', 'stylelint-config.json', '--syntax', 'scss']
 ));
+
+/** Task to run TSLint against the e2e/ and src/ directories. */
 gulp.task('tslint', execNodeTask('tslint', ['-c', 'tslint.json', 'src/**/*.ts']));

7a9c1468c59a851b4eb442eea031e1cbb834d5a6
dharmeshpipariya <pipariyadharmesh@gmail.com>
2017-02-18 10:07:20 +0530

    chore(dialog) update animations

--- src/lib/dialog/dialog.ts
+++ src/lib/dialog/dialog.ts
@@ -8,6 +8,11 @@ import {
   OnDestroy,
   Directive,
   ViewContainerRef,
+  style,
+  trigger,
+  state,
+  transition,
+  animate,
   TemplateRef,
   NgModule,
   ModuleWithProviders
@@ -21,10 +26,9 @@ import {
   TemplatePortalDirective
 } from '../core/core';
 import { Subscription } from 'rxjs/Subscription';
-
 import 'rxjs/add/operator/first';
 
-import { zoomInContent } from './dialog-animations';
+export type DialogVisibility = 'initial' | 'visible' | 'hidden';
 
 @Directive({ selector: '[md2DialogPortal]' })
 export class Md2DialogPortal extends TemplatePortalDirective {
@@ -48,7 +52,16 @@ export class Md2DialogFooter { }
     'tabindex': '0',
     '(body:keydown.esc)': '_handleEscKeydown($event)'
   },
-  animations: [zoomInContent],
+  animations: [
+    trigger('state', [
+      state('void', style({ transform: 'scale(0.3)' })),
+      state('initial', style({ transform: 'scale(0.3)' })),
+      state('visible', style({ transform: 'scale(1)' })),
+      state('hidden', style({ transform: 'scale(0.3)' })),
+      transition('* => visible', animate('150ms cubic-bezier(0.0, 0.0, 0.2, 1)')),
+      transition('* => hidden', animate('150ms cubic-bezier(0.4, 0.0, 1, 1)')),
+    ])
+  ],
   encapsulation: ViewEncapsulation.None,
   exportAs: 'md2Dialog'
 })
@@ -57,6 +70,9 @@ export class Md2Dialog implements OnDestroy {
   private _panelOpen = false;
   private _overlayRef: OverlayRef = null;
   private _backdropSubscription: Subscription;
+
+  /** Property watched by the animation framework to show or hide the dialog */
+  _visibility: DialogVisibility = 'initial';
 
   constructor(private _overlay: Overlay) { }
 
@@ -79,11 +95,13 @@ export class Md2Dialog implements OnDestroy {
     this._overlayRef.attach(this._portal);
     this._subscribeToBackdrop();
     this._panelOpen = true;
+    this._visibility = 'visible';
     return Promise.resolve<Md2Dialog>(this);
   }
 
   /** Close the dialog */
   close(): Promise<Md2Dialog> {
+    this._visibility = 'hidden';
     this._panelOpen = false;
     if (this._overlayRef) {
       this._overlayRef.detach();

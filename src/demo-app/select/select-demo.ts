562c21f117d6fb90bfb86483545450baa0663413
dharmeshpipariya <pipariyadharmesh@gmail.com>
2017-02-27 09:32:44 +0530

    chore(select) update demo

--- src/demo-app/select/select-demo.ts
+++ src/demo-app/select/select-demo.ts
@@ -6,26 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: 'select-demo.html'
 })
 export class SelectDemo {
-  constructor() {
-    setTimeout(() => {
-      this.items.push({ name: 'Manjalpur', value: '7', disabled: false });
-      this.items.push({ name: 'Sitabag', value: '8', disabled: false });
-      this.items.push({ name: 'Vasna', value: '9', disabled: false });
-      this.items.push({ name: 'Gorva', value: '10', disabled: false });
-    }, 3000);
-  }
+  isRequired = false;
+  isDisabled = false;
+  isDisabledMultiple = false;
+  item: any = null;
+  itemMultiple: Array<string> = [];
+
   items: Array<any> =
   [
-    { name: 'Vadodara', value: '1', disabled: false },
-    { name: 'Rajkot', value: '2', disabled: false },
-    { name: 'Delhi', value: '3', disabled: false },
-    { name: 'Chennai', value: '4', disabled: true },
-    { name: 'Mumbai', value: '5', disabled: false },
-    { name: 'Goa', value: '6', disabled: true }
+    { name: 'Vadodara', value: '1' },
+    { name: 'Rajkot', value: '2' },
+    { name: 'Delhi', value: '3' },
+    { name: 'Chennai', value: '4' },
+    { name: 'Mumbai', value: '5' },
+    { name: 'Goa', value: '6' }
   ];
-  multiItem: Array<any> = ['1', '3'];
-  item: string = '3';
-  handleChange(value: any) {
-    console.log('Changed data: ', value);
+  setValue() {
+    this.itemMultiple = ['2'];
   }
 }

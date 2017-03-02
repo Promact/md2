656601f9cb3cffbe11ac9ef3fefe931832e6a368
dharmeshpipariya <pipariyadharmesh@gmail.com>
2017-03-02 12:25:18 +0530

    chore(datepicker) update calendar

--- src/lib/datepicker/calendar.ts
+++ src/lib/datepicker/calendar.ts
@@ -6,6 +6,7 @@ import {
   EventEmitter,
   ViewEncapsulation,
 } from '@angular/core';
+import { DateLocale } from './date-locale';
 
 @Component({
   moduleId: module.id,
@@ -17,13 +18,18 @@ import {
   },
   //encapsulation: ViewEncapsulation.None
 })
-export class Md2Clock {
+export class Md2Calendar {
 
   private _selected: Date;
+  private _date: Date;
+
+  _days: Array<any> = this._locale.days;
+
+  dates: Array<any> = [];
 
   _view: boolean = true;
 
-  constructor(private _element: ElementRef) {
+  constructor(private _element: ElementRef, private _locale: DateLocale) {
     this.renderCalendar();
   }
 
@@ -34,6 +40,13 @@ export class Md2Clock {
   set selected(value: Date) {
     if (this._selected !== value) {
       this._selected = value || new Date();
+    }
+  }
+
+  get date() { return this._date; }
+  set date(value: Date) {
+    if (this._date !== value) {
+      this._date = value || new Date();
     }
   }
 
@@ -53,7 +66,69 @@ export class Md2Clock {
    * render Calendar
    */
   private renderCalendar() {
+    //let year = this.date.getFullYear();
+    //let month = this.date.getMonth();
 
+    //this.dates.length = 0;
+
+    //let firstDayOfMonth = this._locale.getFirstDateOfMonth(this.date);
+    //let numberOfDaysInMonth = this._locale.getNumberOfDaysInMonth(this.date);
+    //let numberOfDaysInPrevMonth = this._locale.getNumberOfDaysInMonth(
+    //  this._locale.incrementMonths(this.date, -1));
+
+    //let dayNbr = 1;
+    ////let calMonth = this._prevMonth;
+    //if (i === 1) {
+    //  let prevMonth = numberOfDaysInPrevMonth - firstDayOfMonth.getDay() + 1;
+    //  for (let j = prevMonth; j <= numberOfDaysInPrevMonth; j++) {
+    //    let iDate: IDate = { year: year, month: month - 1, day: j, hour: 0, minute: 0 };
+    //    let date: Date = new Date(year, month - 1, j);
+    //    week.push({
+    //      date: date,
+    //      dateObj: iDate,
+    //      calMonth: calMonth,
+    //      today: this._locale.isSameDay(this.today, date),
+    //      disabled: this._isDisabledDate(date)
+    //    });
+    //  }
+
+    //  calMonth = this._currMonth;
+    //  let daysLeft = 7 - week.length;
+    //  for (let j = 0; j < daysLeft; j++) {
+    //    let iDate: IDate = { year: year, month: month, day: dayNbr, hour: 0, minute: 0 };
+    //    let date: Date = new Date(year, month, dayNbr);
+    //    week.push({
+    //      date: date,
+    //      dateObj: iDate,
+    //      calMonth: calMonth,
+    //      today: this._locale.isSameDay(this.today, date),
+    //      disabled: this._isDisabledDate(date)
+    //    });
+    //    dayNbr++;
+    //  }
+    //} else {
+    //  for (let j = 1; j < 8; j++) {
+    //    if (dayNbr > numberOfDaysInMonth) {
+    //      dayNbr = 1;
+    //      calMonth = this._nextMonth;
+    //    }
+    //    let iDate: IDate = {
+    //      year: year,
+    //      month: calMonth === this._currMonth ? month : month + 1,
+    //      day: dayNbr, hour: 0, minute: 0
+    //    };
+    //    let date: Date = new Date(year, iDate.month, dayNbr);
+    //    week.push({
+    //      date: date,
+    //      dateObj: iDate,
+    //      calMonth: calMonth,
+    //      today: this._locale.isSameDay(this.today, date),
+    //      disabled: this._isDisabledDate(date)
+    //    });
+    //    dayNbr++;
+    //  }
+    //  this.dates.push(week);
+    //}
   }
 
 }

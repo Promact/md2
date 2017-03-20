94864e90b280b9c6b46c8c387fbd99a184753e06
dharmeshpipariya <pipariyadharmesh@gmail.com>
2017-03-20 12:21:02 +0530

    chore(colorpicker) update demo

--- src/demo-app/colorpicker/colorpicker-demo.ts
+++ src/demo-app/colorpicker/colorpicker-demo.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
-    selector: 'colorpicker-demo',
-    templateUrl: 'colorpicker-demo.html'
+  moduleId: module.id,
+  selector: 'colorpicker-demo',
+  templateUrl: 'colorpicker-demo.html'
 })
 export class ColorpickerDemo {
-    _color: string = null;
-    isRequired = false;
-    isDisabled = false;
-    container: string = 'inline';
-    containers: Array<any> = [
-        { text: 'Inline', value: 'inline' },
-        { text: 'Dialog', value: 'dialog' }];
-    handleChange(value: any) {
-        console.log('Changed color: ', value);
-    }
+  _color: string = null;
+  isRequired = false;
+  isDisabled = false;
+  container: string = 'inline';
+  containers: Array<any> = [
+    { text: 'Inline', value: 'inline' },
+    { text: 'Dialog', value: 'dialog' }];
 }

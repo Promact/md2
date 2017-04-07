4fd1ff8112ad59e37387c1216d445c9fc7a5622e
dharmeshpipariya <pipariyadharmesh@gmail.com>
2017-04-07 14:26:16 +0530

    chore: update rollup config

--- src/demo-app/rollup-config.js
+++ src/demo-app/rollup-config.js
@@ -1,21 +1,19 @@
-﻿import rollup from 'rollup'
-import nodeResolve from 'rollup-plugin-node-resolve'
+﻿import rollup from 'rollup';
+import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import uglify from 'rollup-plugin-uglify'
+import uglify from 'rollup-plugin-uglify';
 
 export default {
-  entry: 'dist/main-aot.js',
+  entry: 'dist/main-aot.ts',
   dest: 'dist/build.js', // output a single application bundle
   sourceMap: false,
   format: 'iife',
   onwarn: function (warning) {
     // Skip certain warnings
-
     // should intercept ... but doesn't in some rollup versions
     if (warning.code === 'THIS_IS_UNDEFINED') { return; }
     // intercepts in some rollup versions
-    if (warning.indexOf("The 'this' keyword is equivalent to 'undefined'") > -1) { return; }
-
+    //if (warning.indexOf("The 'this' keyword is equivalent to 'undefined'") > -1) { return; }
     // console.warn everything else
     console.warn(warning.message);
   },

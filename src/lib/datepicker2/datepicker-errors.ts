7ca75fdbe57536287be563bad88b2573cc9a5bff
Dharmesh Pipariya <dharmesh@promactinfo.com>
2017-05-30 13:55:34 +0530

    chore: update datepicker

--- src/lib/datepicker2/datepicker-errors.ts
+++
@@ -1,6 +0,0 @@
-/** @docs-private */
-export function createMissingDateImplError(provider: string) {
-  return new Error(
-      `Md2Datepicker: No provider found for ${provider}. You must import one of the following` +
-      `modules at your application root: Md2NativeDateModule, or provide a custom implementation.`);
-}

13276bfac235a44e78267b34a4d1c295750a3dfa
Dharmesh Pipariya <pipariyadharmesh@gmail.com>
2017-06-02 12:29:41 +0530

    chore: implemented month, year and calendar body components in datepicker

--- src/lib/datepicker/index.ts
+++ src/lib/datepicker/index.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { OverlayModule, PortalModule } from '../core';
 import { Md2Datepicker } from './datepicker';
 import { Md2Calendar } from './calendar';
+import { Md2MonthView } from './month-view';
+import { Md2YearView } from './year-view';
+import { Md2CalendarBody } from './calendar-body';
 import { Md2Clock } from './clock';
 import { DateUtil } from './date-util';
 import { DateLocale } from './date-locale';
@@ -10,6 +13,9 @@ import { StyleModule } from '../core/style/index';
 
 
 export * from './datepicker';
+export * from './month-view';
+export * from './year-view';
+export * from './calendar-body';
 export * from './clock';
 export * from './date-util';
 export * from './date-locale';
@@ -25,11 +31,17 @@ export * from './date-locale';
   exports: [
     Md2Datepicker,
     Md2Calendar,
+    Md2MonthView,
+    Md2YearView,
+    Md2CalendarBody,
     Md2Clock,
   ],
   declarations: [
     Md2Datepicker,
     Md2Calendar,
+    Md2MonthView,
+    Md2YearView,
+    Md2CalendarBody,
     Md2Clock,
   ],
   providers: [DateLocale, DateUtil],

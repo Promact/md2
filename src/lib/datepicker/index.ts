66c7c4de7f1e7781dfb69f1d8cd3933b3b58aff7
Dharmesh Pipariya <pipariyadharmesh@gmail.com>
2017-06-12 12:09:38 +0530

    chore(datepicker) update locale service for internationalisation with locale and intl object

--- src/lib/datepicker/index.ts
+++ src/lib/datepicker/index.ts
@@ -18,8 +18,8 @@ export * from './month-view';
 export * from './year-view';
 export * from './calendar-body';
 export * from './clock';
-export * from './date-util';
 export * from './date-locale';
+export * from './date-util';
 
 
 @NgModule({

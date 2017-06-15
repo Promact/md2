e6eaa1485fe0256869a59fc783b4ba26a3e680a7
Dharmesh Pipariya <pipariyadharmesh@gmail.com>
2017-06-15 17:49:38 +0530

    chore: update deploy build script

--- scripts/ci/publish-artifacts.sh
+++ scripts/ci/publish-artifacts.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
 # Script that runs after the testing stage of Travis passed.
-# Build artifacts and docs content will be published to different repositories.
+# Build artifacts and demo content will be published to different repositories.
 
 # Go to the project root directory
 cd $(dirname $0)/../..
 
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-  echo "Build artifacts and docs content will only be deployed in Travis push builds."
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]] || [[ "$TRAVIS_BRANCH" != "master" ]]; then
+  echo "Build artifacts and demo content will only be deployed in Travis push builds and branch is master."
   exit 0;
 fi
 
-echo "Starting to publish the build artifacts and docs content..."
-
-# Build Md2 before publishing
-$(npm bin)/gulp build:release
-
+echo "Starting to publish the build artifacts and demo content..."
 # Run publishing.
-./scripts/deploy/publish-build-artifacts.sh --no-build &
+bash ./scripts/deploy/publish-build-artifacts.sh --no-build &
 
 # Deploy the demo.
-./scripts/deploy/deploy-demo.sh &
+bash ./scripts/deploy/deploy-demo.sh &
 
 wait

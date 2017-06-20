3427fb1c8d6a7a6692224a3729ffba2a094a0444
Dharmesh Pipariya <pipariyadharmesh@gmail.com>
2017-06-20 12:39:01 +0530

    chore: updated tasks for AOT

--- tools/gulp/tasks/aot.ts
+++ tools/gulp/tasks/aot.ts
@@ -3,19 +3,19 @@ import {join} from 'path';
 import {DIST_ROOT} from '../constants';
 import {execNodeTask, sequenceTask} from '../util/task_helpers';
 
-/** Copies the source files of the demo-app to the dist folder. */
-task('aot:copy', [':build:devapp:vendor', ':build:devapp:scss', ':build:devapp:assets']);
-
 /**
  * Prepares the AOT compilation by copying the demo-app and building the components with their
  * associated metadata files from the Angular compiler.
  */
-task('aot:prepare', sequenceTask(
-  'clean',
-  ['aot:copy', 'build:components', ':build:components:ngc'])
+task('aot:deps', sequenceTask(
+  'build:devapp',
+  ':build:components:release')
 );
 
-/** Builds the demo-app with the Angular compiler to verify that all components work. */
-task('aot:build', ['aot:prepare'], execNodeTask(
-  '@angular/compiler-cli', 'ngc', ['-p', join(DIST_ROOT, 'tsconfig-aot.json')])
-);
+/** Build the demo-app and a release to confirm that the library is AOT-compatible. */
+task('aot:build', sequenceTask('aot:deps', 'aot:compiler-cli'));
+
+/** Build the demo-app and a release to confirm that the library is AOT-compatible. */
+task('aot:compiler-cli', execNodeTask(
+  '@angular/compiler-cli', 'ngc', ['-p', join(DIST_ROOT, 'tsconfig-aot.json')]
+));
